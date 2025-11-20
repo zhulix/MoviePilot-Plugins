fc3ea6da1810ccca8fbcd6fb55bce794049f20a3
zhulixin <zhulx@tuyuansu.com.cn>
2025-11-20 20:20:07 +0800

    feat:增加事件通知

--- plugins.v2/javauploadernotifier/__init__.py
+++ plugins.v2/javauploadernotifier/__init__.py
@@ -26,7 +26,7 @@ class JavaUploaderNotifier(_PluginBase):
     # 插件图标
     plugin_icon = "Webdav_A.png"
     # 插件版本
-    plugin_version = "1.5"
+    plugin_version = "1.6"
     # 插件作者
     plugin_author = "zhulixin"
     # 作者主页
